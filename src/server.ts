f0bc698ab695eeb2397199a680ef65e42ac4f168
Omar Obregon <oobregon544@gmail.com>
2024-07-20 21:32:45 -0500

    placing the connection link in environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
 import router from './router';
+import db from './config/db';
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log('Successful connection to the database');
+    } catch (error) {
+        console.log(error);
+        console.log('There was an error connecting to the database');
+    }
+}
+
+connectDB();
 
 const server = express();
 
