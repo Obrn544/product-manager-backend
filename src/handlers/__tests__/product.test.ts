a1dee84c7bc06e30d137661074881dc11bcaf055
Omar Obregon <oobregon544@gmail.com>
2024-07-22 21:57:56 -0500

    testing validation of creating product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,17 @@ import request from 'supertest';
 import server from '../../server';
 
 describe('POST /api/products', () => {
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({});
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(4);
+
+        expect(response.status).not.toBe(404);
+        expect(response.body.errors).not.toHaveLength(2);
+    });
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name: 'Monitor Curvo 34 pulgadas',
@@ -11,8 +22,8 @@ describe('POST /api/products', () => {
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('data');
 
-        expect(response.status).toBe(404);
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty('error');
+        expect(response.status).not.toBe(404);
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('errors');
     });
 });
