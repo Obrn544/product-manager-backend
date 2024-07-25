6a764647dcf82a2a22718f3639f95f45ce77321e
Omar Obregon <oobregon544@gmail.com>
2024-07-25 17:50:13 -0500

    testing product elimination

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -182,3 +182,37 @@ describe('PUT /api/products/:id', () => {
         expect(response.body).not.toHaveProperty('errors');
     });
 });
+
+describe('DELETE /api/products/:id', () => {
+    it('should check a valid ID', async () => {
+        const response = await request(server).delete(
+            `/api/products/not-valid-url`
+        );
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors[0].msg).toBe('ID no válido');
+    });
+
+    it('should return a 404 response for a non-existent product', async () => {
+        const productId = 4000;
+        const response = await request(server).delete(
+            `/api/products/${productId}`
+        );
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe('Producto no encontrado');
+
+        expect(response.status).not.toBe(200);
+    });
+
+    it('should delete a product', async () => {
+        const response = await request(server).delete(`/api/products/1`);
+
+        expect(response.status).toBe(200);
+        expect(response.body.data).toBe('Producto eliminado');
+
+        expect(response.status).not.toBe(404);
+        expect(response.status).not.toBe(400);
+    });
+});
